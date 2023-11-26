5a2b66441a6e33f42725fda95cc60870107830cb
Elizabeth deVries <Edevries5872@conestogac.on.ca>
2023-11-26 15:32:52 -0500

    initial commit of simulation program

---
+++ PROG3070_TermProject_SimulateStation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG3070_TermProject_SimulateStation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
