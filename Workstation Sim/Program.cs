4dcc97eb9643c89d86b82bc1de766150d9c1ff5d
TectonicH <kristianbiviens@gmail.com>
2023-11-28 13:58:56 -0500

    Forgot to add certain files in the commit

--- Workstation Sim/Program.cs
+++ Workstation Sim/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -12,124 +13,109 @@ namespace Workstation_Sim
 
         static async Task Main(string[] args)
         {
-            Console.Write("Enter the number of workstations to simulate: ");
-            if (!int.TryParse(Console.ReadLine(), out int numberOfWorkstations))
-            {
-                Console.WriteLine("Invalid input. Please enter a number.");
-                return;
-            }
+            Console.WriteLine("Enter the Station ID:"); // Will remove this later 
+            int stationId = int.Parse(Console.ReadLine());
+            string connectionString = "Server=KRISTIANSYOGA;Database=PROG3070_TermProjectDB;Trusted_Connection=True;"; // Will be placing in another file later 
 
-            var tasks = new List<Task>();
-            for (int i = 1; i <= numberOfWorkstations; i++)
-            {
-                tasks.Add(Task.Run(() => SimulateWorkstation(i)));
-            }
-
-            await Task.WhenAll(tasks); // Wait for all tasks to complete before exiting
+            await SimulateWorkstation(stationId, connectionString);
         }
 
-
-        static async Task SimulateWorkstation(int stationId)
+        static async Task SimulateWorkstation(int stationId, string connectionString)
         {
-            Console.WriteLine($"Workstation {stationId} starting simulation...");
-
-            string connectionString = "Server=KRISTIANSYOGA;Database=myDataBase;Trusted_Connection=True";
-
             using (var connection = new SqlConnection(connectionString))
             {
                 await connection.OpenAsync();
-                while (true) // TODO: Replace with condition to stop the simulation
+
+                while (true) // TODO: Gotta replace this with a proper stopping condition
                 {
-                    // Fetch current state and bins
-                    var workstationInfo = await FetchWorkstationInfo(connection, stationId);
-                    if (!workstationInfo.IsActive)
+                    var (success, lampId) = await BeginAssembly(connection, stationId);
+                    if (!success || lampId == -1)
                     {
-                        Console.WriteLine($"Workstation {stationId} is currently inactive.");
-                        break; // Exit the loop if the station isn't active
+                        Console.WriteLine($"Error at workstation {stationId}. Unable to begin assembly.");
+                        break; 
                     }
 
-                    // Simulate the assembly of a new lamp
-                    // Create the new lamp record in the database and get the generated LampID
-                    int lampId = await CreateNewLampRecord(connection, stationId, workstationInfo.CurrentWorkerId);
+                    int assemblyTime = await GetAssemblyTime(connection, lampId);
 
-                    // Decrease part quantities in bins and check if the runner needs to be notified
-                    await UpdateBinsAndNotifyRunner(connection, workstationInfo);
+                    // check if assemblyTime is valid
+                    if (assemblyTime <= 0)
+                    {
+                        Console.WriteLine($"Invalid assembly time retrieved for lamp {lampId} at workstation {stationId}.");
+                        break; 
+                    }
 
-                    // Simulate the assembly time based on the worker's skill level
-                    int assemblyTime = CalculateAssemblyTime(workstationInfo.WorkerSkillLevel);
-                    await Task.Delay(assemblyTime * 1000); // Convert seconds to milliseconds
+                    Console.WriteLine($"Assembling lamp {lampId} for {assemblyTime} seconds...");
+                    await Task.Delay(assemblyTime * 1000); // Simulate assembly time
 
-                    // Update the lamp status after assembly
-                    string lampStatus = DetermineLampStatus(workstationInfo.DefectRate);
-                    await UpdateLampStatus(connection, lampId, lampStatus, assemblyTime);
+                    await FinishAssembly(connection, lampId);
+                    Console.WriteLine($"Finished assembling lamp {lampId}.");
 
-                    // Log completion and wait before starting the next lamp
-                    Console.WriteLine($"Workstation {stationId} has completed assembly of lamp {lampId} with status: {lampStatus}");
-                    await Task.Delay(1000); // Wait for a second before the next assembly
+                    await Task.Delay(1000); // Delay before next assembly
                 }
-                Console.WriteLine($"Workstation {stationId} simulation ending...");
             }
         }
 
-        // Fetch the current state and associated bin info for the workstation
-        static async Task<WorkstationInfo> FetchWorkstationInfo(SqlConnection connection, int stationId)
+
+        static async Task<(bool Success, int LampId)> BeginAssembly(SqlConnection connection, int stationId)
         {
-            // TODO: Implement the SQL query and return the workstation info
-            // TODO: Add implementation
-            return new WorkstationInfo
+            using (var command = new SqlCommand("BeginAssembly", connection))
             {
-                IsActive = true,
-                CurrentWorkerId = 1,
-                WorkerSkillLevel = "Intermediate",
-                // TODO: decide if there are other properties
-            };
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@stationID", stationId);
+                var lampIdParam = new SqlParameter("@lampID", SqlDbType.Int) { Direction = ParameterDirection.Output };
+                command.Parameters.Add(lampIdParam);
+
+                try
+                {
+                    await command.ExecuteNonQueryAsync();
+                    int lampId = (int)lampIdParam.Value;
+                    return (true, lampId);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error in BeginAssembly: {ex.Message}");
+                    return (false, -1);
+                }
+            }
         }
 
-        // Create a new lamp record in the FogLamps table and return the LampID
-        static async Task<int> CreateNewLampRecord(SqlConnection connection, int stationId, int workerId)
+        static async Task FinishAssembly(SqlConnection connection, int lampId)
         {
-            // TODO: Implement the SQL INSERT command and return the new LampID
-            // TODO: Add implementation
-            return 1; // Dummy LampID
+            using (var command = new SqlCommand("FinishAssembly", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@lampID", lampId);
+
+                try
+                {
+                    await command.ExecuteNonQueryAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error in FinishAssembly: {ex.Message}");
+                }
+            }
         }
 
-        // Update bin quantities and notify the runner if needed
-        static async Task UpdateBinsAndNotifyRunner(SqlConnection connection, WorkstationInfo workstationInfo)
+        static async Task<int> GetAssemblyTime(SqlConnection connection, int lampId)
         {
-            // TODO: Implement the SQL UPDATE command for bins and INSERT command for runner tasks
-            // TODO: Add implementation
+            using (var command = new SqlCommand("SELECT dbo.GetAssemblyTime(@lampID)", connection))
+            {
+                command.Parameters.AddWithValue("@lampID", lampId);
+
+                try
+                {
+                    var result = await command.ExecuteScalarAsync();
+                    return result != DBNull.Value ? Convert.ToInt32(result) : -1;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error fetching assembly time: {ex.Message}");
+                    return -1;
+                }
+            }
         }
 
-        // Calculate assembly time based on worker's skill level
-        static int CalculateAssemblyTime(string workerSkillLevel)
-        {
-            // TODO: Implement logic to determine assembly time based on skill level
-            // TODO: Add implementation
-            return 60; // Default to 60 seconds
-        }
-
-        // Determine if the assembled lamp is defective
-        static string DetermineLampStatus(decimal defectRate)
-        {
-            // TODO: Implement logic to determine if lamp is defective based on defect rate
-            // TODO: Add implementation
-            return new Random().NextDouble() < (double)defectRate ? "Defective" : "Completed";
-        }
-
-        // Update the lamp status in the FogLamps table
-        static async Task UpdateLampStatus(SqlConnection connection, int lampId, string status, int assemblyTime)
-        {
-            // TODO: Implement the SQL UPDATE command for the lamp status
-            // TODO: Add implementation
-        }
-    }
-
-    class WorkstationInfo
-    {
-        public bool IsActive { get; set; }
-        public int CurrentWorkerId { get; set; }
-        public string WorkerSkillLevel { get; set; }
-        public decimal DefectRate { get; set; }
     }
 
 }
